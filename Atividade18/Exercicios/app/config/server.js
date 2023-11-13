4e92c84a7f2f2b6184be4a891317c89fd39081a3
John Wesley Faria de Brito <111017693+JOHNBRITO25@users.noreply.github.com>
2023-11-13 12:42:16 -0300

    Delete Atividade18/Exercicios directory

--- Atividade18/Exercicios/app/config/server.js
+++
@@ -1,5 +0,0 @@
-var express = require('express'); 
-var app=express(); // executando o express 
-app.set('view engine', 'ejs'); // o mecanismo de engine a ser usado
-app.set('views','./app/views'); // diretório onde os arquivos estão localizados
-module.exports = app; 
